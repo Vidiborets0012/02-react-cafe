2232f18279473bb0453719214a63722b03690c95
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-02 12:57:39 +0200

    add the VoteOptions component, declare the VoteOptionsProps interface

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,34 @@
 import { useState } from 'react';
 import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
 import type { Votes, VoteType } from '../../types/votes';
+
 import css from './App.module.css';
 
 export default function App() {
-  // Стан з кількістю голосів
   const [votes, setVotes] = useState<Votes>({
     good: 0,
     neutral: 0,
     bad: 0,
   });
 
-  // ✅ Оновлення стану голосів
+  // console.log(votes);
+
   const handleVote = (type: VoteType) => {
     setVotes(prevVotes => ({
       ...prevVotes,
       [type]: prevVotes[type] + 1,
     }));
   };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
     </div>
   );
 }
