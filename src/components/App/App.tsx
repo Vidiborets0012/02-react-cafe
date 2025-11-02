cad1e675c1a4ae10c7eceaa306ac90b73ef88c31
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-02 11:45:26 +0200

    add and type the application state (votes), add functions for working with the state

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import CafeInfo from '../CafeInfo/CafeInfo';
+import type { Votes, VoteType } from '../../types/votes';
 import css from './App.module.css';
 
 export default function App() {
+  // Стан з кількістю голосів
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  // ✅ Оновлення стану голосів
+  const handleVote = (type: VoteType) => {
+    setVotes(prevVotes => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
   return (
     <div className={css.app}>
       <CafeInfo />
