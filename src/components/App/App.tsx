6576ea36834f066c3fec272fc715990037242bb7
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-02 13:45:13 +0200

    add to the App component the calculation of the total number of votes collected from all categories and the percentage of positive reviews.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -24,11 +24,20 @@ export default function App() {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
 
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
   return (
     <div className={css.app}>
       <CafeInfo />
       <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
-      <VoteStats votes={votes} totalVotes={0} positivRate={0} />
+      <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positivRate={positiveRate}
+      />
     </div>
   );
 }
